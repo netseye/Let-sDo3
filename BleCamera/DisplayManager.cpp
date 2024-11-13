915239a89a1968a3379a8b28514c78bb9504dff1
netseye <netseye@gmail.com>
2024-11-13 20:32:21 +0800

    use canvas

--- BleCamera/DisplayManager.cpp
+++ BleCamera/DisplayManager.cpp
@@ -31,50 +31,9 @@ void tftInit() {
 }
 
 void flashScreen() {
-   tft.fillScreen(ST77XX_WHITE);
-    delay(500);
+  tft.fillScreen(ST77XX_WHITE);
+  delay(500);
 }
-
-// void displayParameters(int aperture, int shutter_speed, int lux, int iso, ExposureMode exposureMode) {
-
-//   static float lastAperture = -1;
-//   static int lastShutterSpeed = -1;
-//   static unsigned int lastLux = 0;
-//   static float lastIso = -1;
-//   static bool lastDeviceConnected = false;
-
-//   if (lastAperture != aperture || lastShutterSpeed != shutter_speed || lastLux != lux || lastIso != iso || lastDeviceConnected != deviceConnected) {
-
-//     lastAperture = aperture;
-//     lastShutterSpeed = shutter_speed;
-//     lastLux = lux;
-//     lastIso = iso;
-//     lastDeviceConnected = deviceConnected;
-
-//     tft.fillScreen(deviceConnected ? ST77XX_BLUE : ST77XX_BLACK);
-//     tft.setTextColor(ST77XX_WHITE);
-//     tft.setTextSize(2);
-
-//     // 显示光圈值
-//     tft.setCursor(10, 20);
-//     tft.printf("Aperture: f/%.1f", aperture / 10.0f);
-
-//     // 显示快门速度
-//     tft.setCursor(10, 50);
-//     tft.printf("Shutter: 1/%d", shutter_speed);
-
-//     // 显示 Lux 值
-//     tft.setCursor(10, 80);
-//     tft.printf("Lux: %u", lux);
-
-//     // 显示 ISO 值
-//     tft.setCursor(10, 110);
-//     tft.printf("ISO: %.2f", iso);
-//   }
-
-//     //  display.drawRGBBitmap(0, 0, canvas.getBuffer(), 240, 135);
-// }
-
 
 void displayParameters(int aperture, int shutter_speed, int lux, int iso, ExposureMode exposureMode) {
 
@@ -83,9 +42,9 @@ void displayParameters(int aperture, int shutter_speed, int lux, int iso, Exposu
   static unsigned int lastLux = 0;
   static float lastIso = -1;
   static bool lastDeviceConnected = false;
-  static ExposureMode lastExposureMode= AUTO;
+  static ExposureMode lastExposureMode = AUTO;
 
-  if (lastAperture != aperture || lastShutterSpeed != shutter_speed || lastLux != lux || lastIso != iso || lastDeviceConnected != deviceConnected ||lastExposureMode != exposureMode) {
+  if (lastAperture != aperture || lastShutterSpeed != shutter_speed || lastLux != lux || lastIso != iso || lastDeviceConnected != deviceConnected || lastExposureMode != exposureMode) {
 
     lastAperture = aperture;
     lastShutterSpeed = shutter_speed;
@@ -94,26 +53,29 @@ void displayParameters(int aperture, int shutter_speed, int lux, int iso, Exposu
     lastDeviceConnected = deviceConnected;
     lastExposureMode = exposureMode;
 
-    // 设置背景颜色
-    tft.fillScreen(deviceConnected ? ST77XX_BLUE : ST77XX_BLACK);
-    tft.setTextColor(ST77XX_WHITE);
-    tft.setTextSize(2);
+
+    // 使用 canvas 清除背景
+    canvas.fillScreen(deviceConnected ? ST77XX_BLUE : ST77XX_BLACK);
+
+    // 设置字体颜色和大小
+    canvas.setTextColor(ST77XX_WHITE);
+    canvas.setTextSize(2);
 
     // 显示光圈值
-    tft.setCursor(10, 5);
-    tft.printf("Aperture: f/%.1f", aperture / 10.0f);
+    canvas.setCursor(10, 5);
+    canvas.printf("Aperture: f/%.1f", aperture / 10.0f);
 
     // 显示快门速度
-    tft.setCursor(10, 35);
-    tft.printf("Shutter: 1/%d", shutter_speed);
+    canvas.setCursor(10, 35);
+    canvas.printf("Shutter: 1/%d", shutter_speed);
 
     // 显示 Lux 值
-    tft.setCursor(10, 65);
-    tft.printf("Lux: %u", lux);
+    canvas.setCursor(10, 65);
+    canvas.printf("Lux: %u", lux);
 
     // 显示 ISO 值
-    tft.setCursor(10, 95);
-    tft.printf("ISO: %d", iso);
+    canvas.setCursor(10, 95);
+    canvas.printf("ISO: %d", iso);
 
     // 在右上角显示曝光模式
     const char* modeStr = "";
@@ -121,25 +83,27 @@ void displayParameters(int aperture, int shutter_speed, int lux, int iso, Exposu
 
     switch (exposureMode) {
       case AUTO:
-        modeStr = "AE";  // 自动曝光
+        modeStr = "AE";        // 自动曝光
         bgColor = ST77XX_RED;  // 红色背景
         break;
       case APERTURE:
-        modeStr = "AV";  // 光圈优先
+        modeStr = "AV";          // 光圈优先
         bgColor = ST77XX_GREEN;  // 绿色背景
         break;
       case SHUTTER:
-        modeStr = "TV";  // 快门优先
+        modeStr = "TV";            // 快门优先
         bgColor = ST77XX_MAGENTA;  // 紫色背景
         break;
     }
 
-    // 设置右上角显示区域的背景色
-    tft.fillRect(240 - 40, 0, 40, 20, bgColor);
-    tft.setCursor(240 - 35, 5);  // 设置文本位置
-    tft.setTextColor(ST77XX_WHITE);  // 文字为白色
-    tft.setTextSize(2);
-    tft.print(modeStr);
-    delay(16);
+    // 设置右上角曝光模式背景色
+    canvas.fillRect(240 - 40, 0, 40, 20, bgColor);
+    canvas.setCursor(240 - 35, 5);      // 设置文本位置
+    canvas.setTextColor(ST77XX_WHITE);  // 文字为白色
+    canvas.setTextSize(2);
+    canvas.print(modeStr);
+
+    // 将 canvas 的内容绘制到屏幕
+    tft.drawRGBBitmap(0, 0, canvas.getBuffer(), 240, 135);
   }
 }
